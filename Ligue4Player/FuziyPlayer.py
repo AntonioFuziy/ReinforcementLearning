06eecc87edf32fd40a924c9fdb19cc3b396d5e2d
AntonioFuziy <antoniovf@al.insper.edu.br>
2022-04-18 21:57:59 -0300

    feat: init Ligue4PopOut player

---
+++ Ligue4Player/FuziyPlayer.py
@@ -0,0 +1,20 @@
+from Player import Player
+
+class FuziyPlayer(Player):
+  def name():
+    return "Fuziy"
+  
+  def move(self, player_code, board):
+    pass
+
+  def sucessores(self, player_code, board):
+    pass
+
+  def opponent(self, player):
+    pass
+
+  def eval(self, player, board):
+    pass
+
+  def movement(self, player_code, board):
+    pass
